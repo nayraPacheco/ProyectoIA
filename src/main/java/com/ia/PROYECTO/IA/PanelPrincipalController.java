c11cf0e9d388825b9db821d025d6d7b1524c1b82
nayraPacheco <202202557@est.umss.edu>
2025-06-01 01:28:19 -0400

    feat: mostrar tablero de sensores

--- src/main/java/com/ia/PROYECTO/IA/PanelPrincipalController.java
+++ src/main/java/com/ia/PROYECTO/IA/PanelPrincipalController.java
@@ -1,12 +1,21 @@
 package com.ia.PROYECTO.IA;
 
+import com.ia.PROYECTO.IA.models.Sensor;
+import com.ia.PROYECTO.IA.services.sensorServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import java.util.List;
 
 @Controller
 public class PanelPrincipalController {
+    @Autowired
+    private sensorServices sensorServices;
     @GetMapping("/PantallaPrincipal")
-    public String PanelPrincipal() {
+    public String PanelPrincipal(Model model) {
+        List<Sensor> sensores = sensorServices.getAllSensors();
+        model.addAttribute("sensores", sensores);
         return "PantallaPrincipal";
     }
 }
--- src/main/java/com/ia/PROYECTO/IA/controllers/sensorController.java
+++ src/main/java/com/ia/PROYECTO/IA/controllers/sensorController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.List;
 
 @Controller
@@ -27,6 +29,20 @@ public class sensorController {
         model.addAttribute("sensores",sensores);
         return "Sensores";
     }
+    @GetMapping("/api/promedio_humedad")
+    @ResponseBody
+    public double getPromedioHumedad(){
+        List<Sensor> sensores = sensorServices.getAllSensors();
+        if(sensores.isEmpty()){
+            return 0.0;
+        }
+        double sumHumedad = sensores.stream().filter(sensor -> sensor.getSensorType() != null && sensor.getSensorType().toString().toLowerCase().contains("humidity")).mapToDouble(Sensor::getBateria).sum();
+        System.out.println("SENSORES DE HUMEDAD"+sumHumedad);
+        long cantSensoresHumedad = sensores.stream().filter(sensor -> sensor.getSensorType() != null && sensor.getSensorType().toString().toLowerCase().contains("humidity")).count();
+        return Math.round(sumHumedad/cantSensoresHumedad);
+    }
+
+
 
 
 }
